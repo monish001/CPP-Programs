30a4ac2265bbd56337f80d24b900806ce8007e8e
Monish Gupta <monish.gupta1@gmail.com>
2011-07-10 04:14:02 +0530

    Version 1.0 QP_Downloader.jar

--- General/QPDownloader/FileDownloader.java
+++ General/QPDownloader/FileDownloader.java
@@ -1,6 +1,9 @@
-	import java.io.*;
-	import java.net.URL;
-	import java.net.*;
+//File: FileDownloader.java
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.net.URL;
 	public class FileDownloader
 	{
 		public static void main(String args[])
@@ -9,13 +12,12 @@
 			if(args.length != 2 && !(args[0].equals(""))){
 				System.out.println("Usage: <web-link> <new-filename>");
 			}else{try{
-				//java.io.BufferedInputStream in = new java.io.BufferedInputStream(new URL("http://cl.thapar.edu/qp/CH016.pdf").openStream());
 				if((args[0].substring(7,10)).equals("172"))
 					args[0] = "http://cl.thapar.edu/" + args[0].substring(20);
 				System.out.println("FileDownloader: "+args[0]+" "+args[1]);
 				URL url = new URL(args[0]);
-				java.io.BufferedInputStream in = new java.io.BufferedInputStream(url.openStream());
-				java.io.FileOutputStream fos = new java.io.FileOutputStream(args[1]+".pdf");
+				java.io.BufferedInputStream in = new BufferedInputStream(url.openStream());
+				java.io.FileOutputStream fos = new FileOutputStream(args[1]+".pdf");
 				java.io.BufferedOutputStream bout = new BufferedOutputStream(fos,1024);
 				byte[] data = new byte[1024];
 				int x=0;
