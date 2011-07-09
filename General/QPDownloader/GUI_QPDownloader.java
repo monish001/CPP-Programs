f16dc8a87c537af27d833f93600d7f66ea6ed952
Monish Gupta <monish.gupta1@gmail.com>
2011-07-10 00:20:35 +0530

    TODO: 1.Download Btn is not working 2. Write actionListener for download btn

--- General/QPDownloader/GUI_QPDownloader.java
+++ General/QPDownloader/GUI_QPDownloader.java
@@ -19,33 +19,36 @@ import javax.swing.JTable;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.AbstractButton;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import java.util.Vector;
 import java.util.ArrayList;
 
-class CheckBoxRenderer extends JCheckBox implements TableCellRenderer, ItemListener {
-    public CheckBoxRenderer(Object course) {
-		super((String)course);
-		addItemListener(this);
+class CheckBoxRenderer implements TableCellRenderer, ItemListener {
+    public CheckBoxRenderer() {
     }
 	
       public void itemStateChanged(ItemEvent itemEvent) {
         AbstractButton abstractButton = (AbstractButton)itemEvent.getSource();
         int state = itemEvent.getStateChange();
         if (state == ItemEvent.SELECTED) {
-			//do stuff
+			//cb.setSelected(!cb.isSelected());
+			System.out.println("Checkbox change");
         }
 	  }
     
 	public Component getTableCellRendererComponent(
-                            JTable table, Object status,
+                            JTable table, Object ob,
                             boolean isSelected, boolean hasFocus,
                             int row, int column) {
-        if(!(Boolean)status){
-			setEnabled(false);
+		cb = (JCheckBox)ob;
+        if(cb.getText().equals("")){//if label is ""
+			cb.setEnabled(false);
 		}
-        return this;
+		cb.addItemListener(this);
+        return cb;
     }
+	JCheckBox cb;
 }
 
 public class GUI_QPDownloader extends JFrame{
@@ -58,28 +61,16 @@ public class GUI_QPDownloader extends JFrame{
     }
 
     public void addComponentsToPane(final Container pane) {
-
-		Object[] columnNames;
-		Object[][] data;
-	
-		QPDownloader qpdown = new QPDownloader();
-		columnNames = new String[qpdown.seasonPagesInfo.size()];
+		final QPDownloader qpdown = new QPDownloader();
+		final Object[] columnNames = new String[qpdown.seasonPagesInfo.size()];
 		int i=-1;
 		for(SeasonPage sp: qpdown.seasonPagesInfo){
 			columnNames[++i] = sp.toString();
 		}
-		//columnNames = 		(qpdown.seasonPagesInfo).toArray(columnNames);
-		System.out.println(SeasonPage.courses.size()+" "+qpdown.seasonPagesInfo.size());
-		data = new Boolean[SeasonPage.courses.size()][qpdown.seasonPagesInfo.size()];
-		
-		final ArrayList v = new ArrayList(SeasonPage.courses);
-		table = new JTable(data, columnNames){
-			public TableCellRenderer getCellRenderer(int row, int column) {
-				return new CheckBoxRenderer(v.get(row));
-			}
-		};
-		table.setFillsViewportHeight(true);
 
+		System.out.println(SeasonPage.courses.size()+" "+qpdown.seasonPagesInfo.size());
+		final Object[][] data = new JCheckBox[SeasonPage.courses.size()][qpdown.seasonPagesInfo.size()];
+		final boolean[][] editable = new boolean[SeasonPage.courses.size()][qpdown.seasonPagesInfo.size()];
 		int r=-1;
 		for(String course: SeasonPage.courses){//for each course or row
 			int c = -1;	++r;
@@ -94,13 +85,45 @@ public class GUI_QPDownloader extends JFrame{
 					}
 				}
 				if(exists){
-					data[r][++c]=new Boolean(true);
+					data[r][++c]=new JCheckBox(course, false);
+					editable[r][c] = true;
 				}else{
-					data[r][++c]=new Boolean(false);
+					data[r][++c]=new JCheckBox("", false);
+					editable[r][c] = false;
 				}
 			}
 		}
 
+System.out.println((new JCheckBox()).getClass().toString() + " " + data[0][0].getClass().toString());
+		final ArrayList v = new ArrayList(SeasonPage.courses);
+		table = new JTable(){
+			public TableCellRenderer getCellRenderer(int row, int column) {
+				return new CheckBoxRenderer();
+			}
+		};
+		table.setModel(new DefaultTableModel(data, columnNames){
+			public boolean isCellEditable(int r, int c) {
+				return editable[r][c];
+			}
+			public Class getColumnClass(int columnIndex){
+				return Boolean.class;//(new JCheckBox()).getClass();
+			}
+			public Object getValueAt(int row, int col){
+				return data[row][col];
+			}
+			public void setValueAt(Object value, int row, int col)
+			{
+				/*data[row][col] = (JCheckBox)()value;
+				if(editable[row][col])
+					((JCheckBox)data[row][col]).setSelected(!(((JCheckBox)data[row][col]).isSelected()));*/
+				((JCheckBox)data[row][col]).setSelected((Boolean)value);
+				fireTableCellUpdated(row, col);
+			}
+
+
+
+		});
+		table.setFillsViewportHeight(true);
 		
 		//controls
         JPanel controls = new JPanel();
