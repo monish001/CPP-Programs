5b44d650d8b546003379557ea749b293a72c5e4d
Monish Gupta <monish.gupta1@gmail.com>
2011-08-14 20:29:44 +0530

    Alog assign files names corrected

--- CTCI/SetOfStacks.java
+++
@@ -1,8 +0,0 @@
-//HelloWorld.java
-//INCOMPLETE
-public class SetOfStacks{
-	Arraylist stack = new Arraylist();
-	public static void main(String[] args){
-		System.out.println("Hello World!! I am Monish.");
-	}
-}
