9b899f3b2f6794519a748a52f2f7d0a0112d4d34
Monish Gupta <Monish Gupta>
2016-02-14 15:43:16 +0530

    practice problems re-structured in topic-wise folders

--- InterviewBit/arrays/ReadMatrixInSpiral/main.cpp
+++ InterviewBit/arrays/ReadMatrixInSpiral/main.cpp
@@ -78,10 +78,6 @@ vector<int> spiralOrder(const vector<vector<int> > &A) {
     }
 
 	return result;
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
 }
 
 int main()
